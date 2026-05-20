c0e8bdc245515a3ac785174b44c338375afa81aa
fiston <kalambayifiston7@gmail.com>
2026-05-20 18:05:21 +0100

    feat: a task can be deleted with the cmd delete

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/fiston7-code/todo-cli-cobra/models"
 	"github.com/fiston7-code/todo-cli-cobra/storage"
@@ -10,6 +11,7 @@ import (
 )
 
 var myStore = models.NewStore()
+var taskDesc string
 
 var rootCmd = &cobra.Command{
 	Use:   "todo",
@@ -37,7 +39,7 @@ var addCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1), // Oblige l'utilisateur à donner au moins un titre
 	Run: func(cmd *cobra.Command, args []string) {
 		title := args[0]
-		desc := "Pas de description" // En attendant de gérer les flags plus tard
+		desc := taskDesc
 
 		// 1. On crée la tâche dans le store en mémoire
 		newTask := myStore.CreateTask(title, desc)
@@ -53,10 +55,44 @@ var addCmd = &cobra.Command{
 	},
 }
 
+var deleteCmd = &cobra.Command{
+	Use:   "delete [id]",
+	Short: "Supprime une tâche grâce à son ID",
+	Args:  cobra.ExactArgs(1), // Oblige l'utilisateur à donner exactement 1 argument (l'ID)
+	Run: func(cmd *cobra.Command, args []string) {
+		// 1. Convertir l'argument (string) en entier (int)
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("❌ L'ID doit être un nombre valide.")
+			return
+		}
+
+		// 2. Appeler ta méthode DeleteTask du store
+		err = myStore.DeleteTask(id)
+		if err != nil {
+			fmt.Printf("❌ Erreur : %v\n", err)
+			return
+		}
+
+		// 3. Sauvegarder les modifications sur le disque
+		err = storage.Save(myStore.Tasks)
+		if err != nil {
+			fmt.Printf("Erreur lors de la sauvegarde : %v\n", err)
+			return
+		}
+
+		fmt.Printf("🗑️ Tâche %d supprimée avec succès !\n", id)
+	},
+}
+
 func init() {
 	// 1. On attache la commande list
 	rootCmd.AddCommand(listCmd)
 	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(deleteCmd)
+
+	// On lie le flag à notre variable taskDesc
+	addCmd.Flags().StringVarP(&taskDesc, "desc", "d", "", "La description de la tâche")
 
 	// 2. On charge les tâches via ton package storage
 	existingTasks, err := storage.Load()
